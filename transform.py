1170b3f033ce407762c7d913a8e5ae923d177450
wancat <wancat@pm.me>
2021-06-02 11:37:08 +0800

    Drag circles

--- transform.py
+++ transform.py
@@ -12,6 +12,7 @@ class TransformFrame(tk.Frame):
         self.shape = (800, 800)
         self.grid_rowconfigure(3)
         self.grid_columnconfigure(2)
+        self.selected = None
 
         origin_label = tk.Label(self, text='Origin', font=('Arial', 25))
         origin_label.grid(row=0, column=0)
@@ -25,7 +26,9 @@ class TransformFrame(tk.Frame):
             self, width=self.controller.width // 2, height=self.controller.height, bg='black')
         self.product.grid(row=1, column=1)
 
-        self.bind('<1>', self.on_click)
+        self.origin.tag_bind('vertex', '<Button-1>', self.on_click)
+        self.origin.bind('<Motion>', self.on_motion)
+        self.origin.bind('<ButtonRelease-1>', self.on_release)
         button = tk.Button(self, text='Manage',
                            command=lambda: controller.switch_frame('ManageFrame'))
         button.grid(row=2)
@@ -45,12 +48,28 @@ class TransformFrame(tk.Frame):
         self.product.config(width=width, height=height)
         self.product.create_image(
             (10, 10), anchor=tk.NW, image=self.img)
-        self.circle = self.origin.create_oval(10, 10, 30, 30)
+
+        self.r = r = 10
+        self.vertexes = [
+            self.origin.create_oval(
+                x-r, y-r, x+r, y+r, fill='green', tags=('vertex'))
+            for x, y in [
+                (10, 10), (10, 500), (500, 500), (500, 10)
+            ]
+        ]
+        return
 
     def on_click(self, event):
         print(event, event.x, event.y)
-        self.origin.coords(self.circle, event.x, event.y,
-                           event.x + 20, event.y + 20)
+        self.selected = self.origin.find_closest(event.x, event.y)
+
+    def on_motion(self, event):
+        if self.selected:
+            self.origin.coords(
+                self.selected, (event.x-self.r, event.y-self.r, event.x + self.r, event.y + self.r))
+
+    def on_release(self, event):
+        self.selected = None
 
     def image_size(self, image):
         width = self.controller.width
