e09b737f6e91b2b349ffb7533a3a38b51019cf02
wancat <wancat@pm.me>
2021-06-21 16:55:55 +0800

    Get the range of histogram

--- vertexes.py
+++ vertexes.py
@@ -8,13 +8,32 @@ def detect_vertexes(img):
     return edges
 
 
-def histogram(img, mask):
+def histogram(img, mask, threshold=0.002):
     color = ('b', 'g', 'r')
+    result = {}
     for i, col in enumerate(color):
         histr = cv2.calcHist(
-            images=[img], channels=[i], mask=mask, histSize=[256], ranges=[0, 256])
+            images=[img], channels=[i], mask=mask, histSize=[257], ranges=[0, 256]
+        ) / (img.shape[0] * img.shape[1])
+        begin = 0
+        end = 255
+
+        for i, value in enumerate(histr):
+            if value > threshold:
+                begin = i
+                break
+
+        for i, value in enumerate(np.flip(histr)):
+            if value > threshold:
+                end = 255-i
+                break
+
+        result[col] = [begin, end]
+
         plt.plot(histr, color=col)
         plt.xlim([0, 256])
+
+    print(result)
 
 
 if __name__ == "__main__":
@@ -25,10 +44,11 @@ if __name__ == "__main__":
                        [3718.9062,   489.17468], ], dtype=np.int32)
     mask = np.ones(img.shape[:2], dtype=np.uint8)
     cv2.fillConvexPoly(mask, coords, 0)
+    #mask = 1-mask
+    histogram(img, mask)
 
     img[mask == 0] = 0
 
-    histogram(img, mask)
-    cv2.imshow('', img)
+    #cv2.imshow('', img)
     plt.show()
     cv2.waitKey(0)
